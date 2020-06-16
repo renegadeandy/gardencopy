7ceabced1c9bb61b22f206cbd9d0fa6fedfdc271
Andy Armstrong <andyarmstrong@hotmail.co.uk>
2020-06-16 22:09:58 +0100

    Initiall commit of project
    Added box representing house, terrain as a flat surface, and cylinder who can move around in an FPS style.

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 300f;
+    public Transform playerBody;
+
+    private float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Handles looking right and left on x axis
+        playerBody.Rotate(Vector3.up * mouseX);
+       
+        //Handles looking up and down, clamps to 90 degrees up and down looking
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    private Vector3 velocity;
+
+    public float jumpHeight = 1f;
+
+    public float gravity = -9.8f*3;
+    public Transform groundCheck;
+    //radius of sphere we will use to check collisions
+    public float groundDistance = 0.4f;
+    //control which objects this sphere will check for
+    public LayerMask groundMask;
+
+    //are we grounded or not?
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        //Apply our directional movement z is forward / backward and x is left and right (strafe)
+        Vector3 move = (transform.right * x) + (transform.forward * z);
+        controller.Move(move * speed * Time.deltaTime);
+
+        //physics calculation to determine how much velocity we need to jump a certain height
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        //Apply our gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
