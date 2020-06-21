47afb8af8add8d36cabb9beafebd039bb6b9da23
Andy Armstrong <andyarmstrong@hotmail.co.uk>
2020-06-21 01:38:12 +0100

    Added player Ray  when left clicking, detects terrain and house.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Numerics;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
@@ -8,12 +9,13 @@ using Vector3 = UnityEngine.Vector3;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Camera myEyes;
     public float speed = 12f;
     private Vector3 velocity;
 
     public float jumpHeight = 1f;
 
-    public float gravity = -9.8f*3;
+    public float gravity = -29.4f;
     public Transform groundCheck;
     //radius of sphere we will use to check collisions
     public float groundDistance = 0.4f;
@@ -41,7 +43,13 @@ public class PlayerMovement : MonoBehaviour
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        
+
+        //left mouse click
+        if (Input.GetMouseButtonUp(0))
+        {
+            Dig();
+        }
+
         //Apply our directional movement z is forward / backward and x is left and right (strafe)
         Vector3 move = (transform.right * x) + (transform.forward * z);
         controller.Move(move * speed * Time.deltaTime);
@@ -54,7 +62,21 @@ public class PlayerMovement : MonoBehaviour
 
         //Apply our gravity
         velocity.y += gravity * Time.deltaTime;
-        Debug.Log("Velocity is:" + velocity.ToString() + "Gravity deduction is:"+ gravity * Time.deltaTime);
+        //gravity still feels slow.
+       //Debug.Log("Velocity is:" + velocity.ToString() + "Gravity deduction is:"+ gravity * Time.deltaTime);
         controller.Move(velocity * Time.deltaTime);
+    }
+
+
+    void Dig()
+    {
+        Ray ray = myEyes.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0f));
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray,out hit, 50f))
+        {
+            //remove a 'spades' worth of voxels, if pointing at the terrain, and shove them somewhere random nearby.
+            Debug.Log(hit.point);
+        }
     }
 }
