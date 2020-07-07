ec22b348cce67408a061d00cda237d5b10ac538c
Andy Armstrong <andyarmstrong@hotmail.co.uk>
2020-07-07 23:03:48 +0100

    Bug fixes, added boundary checking - still bug remaining where 0 or 64 offset is the edge which creates gaps.

--- Assets/MouseLook.cs
+++ Assets/MouseLook.cs
@@ -6,6 +6,7 @@ using System.Linq;
 //using static MainMeshHolder;
 using static MainChunkHolder;
 using System;
+using System.Net.NetworkInformation;
 
 public class MouseLook : MonoBehaviour
 {
@@ -92,57 +93,260 @@ public class MouseLook : MonoBehaviour
         
                 float[,] heights = td.GetHeights(0, 0, td.heightmapResolution, td.heightmapResolution);
 
-                float[,] newHeights = trowelDig(heights, hitPointTerZ, hitPointTerX);
+                trowelDig(heights, hitPointTerZ, hitPointTerX,piece);
                 
                 
-                td.SetHeightsDelayLOD(0, 0, heights);
+               
                 
             }
         }
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (td != null)
-            {
-                td.SyncHeightmap();
-             
-            }
-        }
+       
     }
 
-    //make this work neater when it hits boundaries of terrain (between terrain pieces and outer edges of terrain :) 
-    private float[,] trowelDig(float[,] heights, int centerPointZ, int centerPointX)
-    {
-        //reduce area 3x3
-        for(int i = centerPointX -2;i <= centerPointX+2; i++)
-        {
-            for(int p = centerPointZ-2; p<= centerPointZ + 2; p++)
-            {
 
-                if (i == centerPointX && p == centerPointZ)
-                {
-                    heights[centerPointZ, centerPointX] = heights[centerPointZ, centerPointX] + trowelDepth;
-                }
-                else
-                {
-                    heights[centerPointZ, centerPointX] = heights[centerPointZ, centerPointX] + (trowelDepth / 2);
-                }
+    public void outputGrid(int centerPointZ, int centerPointX)
+    {
+        for (int x = centerPointX - 2; x <= centerPointX + 2; x++)
+        {
+            String output = "";
+            for (int z = centerPointZ - 2; z <= centerPointZ + 2; z++)
+            {
+               output +="|" + z+","+x;
             }
-         
+            output += "|";
+            Debug.Log(output);
+        }
+    }
+    //make this work neater when it hits boundaries of terrain (between terrain pieces and outer edges of terrain :) 
+    private void trowelDig(float[,] heights, int centerPointZ, int centerPointX,Terrain t)
+    {
+        this.outputGrid(centerPointZ, centerPointX);
+        Debug.Log("Center point Z is:" + centerPointZ + " Center point X is:" + centerPointX);
+
+       
+        for(int x = centerPointX -2;x <= centerPointX+2; x++)
+        {
+            for (int z = centerPointZ - 2; z <= centerPointZ + 2; z++)
+            {
+               
+                //if we are off this tile,get the neighboring piece and adjust that too.
+                Boolean edgeSplitZ = false;
+                Boolean edgeSplitX = false;
+                Terrain chunkZ = null;
+                Terrain chunkX = null;
+                int newXOffset = 0;
+                int newZOffset = 0;
+                this.resetHoleCalcs();
+                //find out if our hit location crosses a terrain boundary
+                if (z > 64)
+                {
+                    chunkZ = this.getNeighbor(t, "top");
+                    newZOffset = 0 + z - 65;
+                    edgeSplitZ = true;
+
+                }
+                else if (z < 0)
+                {
+                    chunkZ = this.getNeighbor(t, "bottom");
+                    newZOffset = 65 + z;
+                    edgeSplitZ = true;
+                }
+                if (x > 64)
+                {
+                    chunkX = this.getNeighbor(t, "right");
+                    newXOffset = 0 + x - 65;
+                    edgeSplitX = true;
+                }
+                else if (x < 0)
+                {
+
+                    chunkX = this.getNeighbor(t, "left");
+                    newXOffset = 65 + x;
+                    edgeSplitX = true;
+                }
+                if(z > 0 && z<= 64 && x > 0 && x <= 64)
+                {
+                    if (z == centerPointX && x == centerPointZ)
+                    {
+                        heights[z, x] = this.getNewHeight("bottomPoint", heights[z, x]);
+                    }
+                    //other surrounding point
+                    else
+                    {
+                        heights[z, x] = this.getNewHeight("slope", heights[z, x]);
+                    }
+                    t.terrainData.SetHeightsDelayLOD(0, 0, heights);
+                    t.terrainData.SyncHeightmap();
+
+                }
+                //handle depth where it crosses a terrain boundary
+                //if (edgeSplitZ || edgeSplitX)
+                // {
+
+
+
+
+                //Handle the case where just x is on the next terrain section
+                if (edgeSplitX && !edgeSplitZ)
+                    {
+                        if (chunkX == null)
+                        {
+                            Debug.Log("Terrain X neighbor doesn't exist - continuing loop");
+                            continue;
+                        }
+                        TerrainData td = chunkX.terrainData;
+                        float[,] otherHeightsX = td.GetHeights(0, 0, td.heightmapResolution, td.heightmapResolution);
+                        Debug.Log("Z offset is:" + z + " new X Offset is:" + newXOffset);
+                        otherHeightsX[z, newXOffset] = this.getNewHeight("slope", otherHeightsX[z, newXOffset]);
+                        td.SetHeightsDelayLOD(0, 0, otherHeightsX);
+                        td.SyncHeightmap();
+                    }
+                    //Handle the case where just z is on the next terrain section
+                    else if (edgeSplitZ && !edgeSplitX)
+                    {
+                        if (chunkZ == null)
+                        {
+                            Debug.Log("Terrain Z neighbor doesn't exist - continuing loop");
+                            continue;
+                        }
+                        TerrainData td = chunkZ.terrainData;
+                        float[,] otherHeightsZ = td.GetHeights(0, 0, td.heightmapResolution, td.heightmapResolution);
+                        Debug.Log("new Z offset is:" + newZOffset + " and X Offset is:" + x);
+                        otherHeightsZ[newZOffset, x] = this.getNewHeight("slope", otherHeightsZ[newZOffset, x]);
+                        td.SetHeightsDelayLOD(0, 0, otherHeightsZ);
+                        td.SyncHeightmap();
+                    }
+                    //check where both z and x are on the next chunk
+                    else
+                    {
+                        if (chunkZ == null || chunkX == null)
+                        {
+                           // Debug.Log("Terrain Z or Terrain X neighbor doesn't exist - continuing loop");
+                            continue;
+                        }
+                        TerrainData td = chunkZ.terrainData;
+                        float[,] otherHeightsZ = td.GetHeights(0, 0, td.heightmapResolution, td.heightmapResolution);
+                        otherHeightsZ[newZOffset, x] = this.getNewHeight("slope", otherHeightsZ[newZOffset, x]); 
+                        td.SetHeightsDelayLOD(0, 0, otherHeightsZ);
+                        TerrainData td2 = chunkX.terrainData;
+                        float[,] otherHeightsX = td2.GetHeights(0, 0, td2.heightmapResolution, td2.heightmapResolution);
+                        otherHeightsX[z, newXOffset] = this.getNewHeight("slope", otherHeightsX[z, newXOffset]);
+                        td2.SetHeightsDelayLOD(0, 0, otherHeightsX);
+                        td.SyncHeightmap();
+                    }
+              //  }
+                //all part of the same terrain piece
+               // else
+               // {
+
+                    //center point
+                  
+              //  }
+            }
         }
 
-        
+
         //Debug.Log("Hit point height before adjusting is:" + heights[centerPointZ, centerPointX]);
         //if (Math.Abs(heights[centerPointZ, centerPointX] - 0.5f) < 0.005f)
-       // {
+        // {
         //    heights[centerPointZ, centerPointX] = heights[centerPointZ, centerPointX] + trowelDepth;
-       //     Debug.Log("Terrain is close to ground level - setting to trowl depth:" + trowelDepth + " we are now at height:" + heights[centerPointZ, centerPointX]);
-       // }
-       // else
-      //  {
-       //     heights[centerPointZ, centerPointX] = heights[centerPointZ, centerPointX] + trowelDepth;
-       //     Debug.Log("Terrain set to:" + heights[centerPointZ, centerPointX]);
-      //  }
-        return heights;
+        //     Debug.Log("Terrain is close to ground level - setting to trowl depth:" + trowelDepth + " we are now at height:" + heights[centerPointZ, centerPointX]);
+        // }
+        // else
+        //  {
+        //     heights[centerPointZ, centerPointX] = heights[centerPointZ, centerPointX] + trowelDepth;
+        //     Debug.Log("Terrain set to:" + heights[centerPointZ, centerPointX]);
+        //  }
+       
+
+        
+    }
+
+    private float slopeVal;
+    private float bottomVal;
+    private Boolean setSlope = false;
+    private Boolean setBottom = false;
+    private float getNewHeight(String area,float original)
+    {
+
+       
+        if (area.Equals("slope"))
+        {
+            if (setSlope)
+            {
+                return slopeVal;
+            }
+            else
+            {
+                slopeVal = original + (trowelDepth / 2);
+                setSlope = true;
+                return slopeVal;
+            }
+               
+        }
+        else if (area.Equals("bottomPoint")){
+            if (setBottom)
+            {
+                return bottomVal;
+            }
+            else
+            {
+                bottomVal = original + trowelDepth;
+                setBottom = true;
+                return bottomVal;
+            }
+        }
+        return 0f;
+    }
+
+    public void resetHoleCalcs()
+    {
+        setSlope = false;
+        setBottom = false;
+    }
+
+    public Terrain getNeighbor(Terrain t, String requestedSide)
+    {
+        if (requestedSide.Equals("top"))
+        {
+            if (t.topNeighbor == null)
+            {
+                return null;
+            }
+            else
+            {
+                return t.topNeighbor.GetComponent<Terrain>();
+            }
+        }else if (requestedSide.Equals("right"))
+        {
+            if( t.rightNeighbor == null)
+            {
+                return null;
+            }else
+            {
+                return t.rightNeighbor.GetComponent<Terrain>();
+            }
+        }else if (requestedSide.Equals("bottom"))
+        {
+            if (t.bottomNeighbor == null)
+            {
+                return null;
+            }
+            else
+            {
+                return t.bottomNeighbor.GetComponent<Terrain>();
+            }
+        }else if (requestedSide.Equals("left"))
+        {
+            if( t.leftNeighbor== null)
+            {
+                return null;
+            }else
+            {
+                return t.leftNeighbor.GetComponent<Terrain>();
+            }
+        }
+        return null;
     }
 
 }
